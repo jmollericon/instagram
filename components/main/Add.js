d1dbd344db143b6d5b02dbfbe736daa136e21bae
Jorge Mollericon <jlmollericon@gmail.com>
2021-07-24 20:36:21 -0400

    using camera

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,12 +1,70 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { Camera } from 'expo-camera'
 
-const Add = () => {
+export default function Add() {
+  const [hasPermission, setHasPermission] = useState(null)
+  const [type, setType] = useState(Camera.Constants.Type.back)
+
+  useEffect(() => {
+    ;(async () => {
+      const { status } = await Camera.requestPermissionsAsync()
+      setHasPermission(status === 'granted')
+    })()
+  }, [])
+
+  if (hasPermission === null) {
+    return <View />
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>
+  }
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Add</Text>
+    <View style={styles.container}>
+      <View style={styles.cameraContainer}>
+        <Camera style={styles.fixedRatio} type={type} ratio={'1:1'} />
+      </View>
+
+      <Button
+        style={styles.button}
+        title="Flip Image"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          )
+        }}
+      />
     </View>
   )
 }
 
-export default Add
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cameraContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+})
